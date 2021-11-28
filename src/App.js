f985523e88c063513165fa4a012f8ce2b85df5cf
stebiskamp <86976063+stebiskamp@users.noreply.github.com>
2021-11-28 20:18:59 +0100

    Add component Forecast, include footer in "App.js" and improve css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import SearchEngine from "./SearchEngine";
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
@@ -9,8 +11,25 @@ function App() {
         <div className="App">
           <SearchEngine />
           <Weather />
+          <div className="row">
+            <Forecast day="Sun" tempMax={7} tempMin={3} />
+            <Forecast day="Mon" tempMax={9} tempMin={0} />
+            <Forecast day="Tue" tempMax={11} tempMin={4} />
+            <Forecast day="Wed" tempMax={7} tempMin={1} />
+            <Forecast day="Thu" tempMax={8} tempMin={4} />
+          </div>
         </div>
       </div>
+      <footer>
+        <a
+          href="https://github.com/stebiskamp/Weather-Project-Stebiskamp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source coded
+        </a>{" "}
+        by Stephanie Biskamp
+      </footer>
     </div>
   );
 }
---
+++ src/Forecast.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function Forecast(props) {
+  return (
+    <div className="col Forecast">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">{props.day}</h5>
+          <i className="fas fa-sun"></i>
+          <p className="card-text">
+            {props.tempMax} / {props.tempMin} °C
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
